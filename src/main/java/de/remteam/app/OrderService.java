0f0333b850ce78661bb8df92cc6c0f1ad33200b7
Matthias Stangl <fazmaz@gmx.de>
2021-07-23 16:30:48 +0200

    removed system.out

--- src/main/java/de/remteam/app/OrderService.java
+++ src/main/java/de/remteam/app/OrderService.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 public class OrderService {
 
-    public static void addOrder(OrderDB currentOrderDB,List<Product> einkaufswagen){
+    public static void addOrder(OrderDB currentOrderDB, List<Product> einkaufswagen) {
         currentOrderDB.addOrder(einkaufswagen);
-        System.out.println("Is korks... kinda");
     }
 
-    public static List<Order> listOrders(OrderDB currentOrderDB){
+    public static List<Order> listOrders(OrderDB currentOrderDB) {
         return currentOrderDB.listOfAllOrders();
     }
 
