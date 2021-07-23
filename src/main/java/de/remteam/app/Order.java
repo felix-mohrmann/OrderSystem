6ed1802c0c3609976324acea0ef6f5656bf2c7e6
Matthias Stangl <fazmaz@gmx.de>
2021-07-23 14:47:24 +0200

    added test for productDB and added Order Class

--- src/main/java/de/remteam/app/Order.java
+++ src/main/java/de/remteam/app/Order.java
@@ -1,4 +1,23 @@
 package de.remteam.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    private final int id;
+    private List<Product> orderedProducts = new ArrayList<>();
+
+    public Order(int id, List<Product> orderedProducts) {
+        this.id = id;
+        this.orderedProducts = orderedProducts;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Product> getOrderedProducts() {
+        return orderedProducts;
+    }
 }
--- src/test/java/de/remteam/app/ProductDBTest.java
+++ src/test/java/de/remteam/app/ProductDBTest.java
@@ -1,7 +1,59 @@
 package de.remteam.app;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductDBTest {
 
+
+    @Test
+    public void testWithAfterDBInit() {
+        // Given
+        ProductDB productDB = new ProductDB();
+
+        // When
+        List<Product> actual = productDB.listOfAllProducts();
+
+        //Then
+        List<Product> expected = List.of(
+                new Product("facebook", 1),
+                new Product("amazon", 2),
+                new Product("netflix", 3),
+                new Product("google", 4)
+        );
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetProductById(){
+        // Given
+        ProductDB productDB = new ProductDB();
+
+        // When
+        Optional<Product> product = productDB.getProductByID(2);
+        Product actual;
+        if(product.isPresent()){
+            actual = product.get();
+            // Then
+            Product expected = new Product("amazon", 2);
+            assertEquals(expected, actual);
+        } else {
+            fail("no Product found by ID returned optional was empty");
+        }
+    }
+
+    @Test
+    public void testGetProductByInvalidId(){
+        // Given
+        ProductDB productDB = new ProductDB();
+
+        // When
+        Optional<Product> product = productDB.getProductByID(5);
+        boolean actual = product.isPresent();
+        assertFalse(actual);
+    }
 }
